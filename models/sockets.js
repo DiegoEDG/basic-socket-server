0662963724172fdb1405e2c685c220aa7cb87055
Digudev <1.5.dg.diego@gmail.com>
2022-12-01 07:52:51 -0600

    Bands modules created

--- models/sockets.js
+++ models/sockets.js
@@ -1,16 +1,32 @@
+const BandList = require('./bandList');
+
 class Sockets {
 	constructor(io) {
 		this.io = io;
+		this.bandList = new BandList();
 
 		this.socketsEvents();
 	}
 
 	socketsEvents() {
 		this.io.on('connection', (socket) => {
-			socket.on('clientMsg', (data) => {
-				console.log(data);
+			console.log('Server connected');
 
-				this.io.emit('msgReturn', data);
+			socket.emit('bandList', this.bandList.getBands());
+
+			socket.on('voteBand', (id) => {
+				this.bandList.increaseVotes(id);
+				this.io.emit('bandList', this.bandList.getBands());
+			});
+
+			socket.on('removeBand', (id) => {
+				this.bandList.removeBand(id);
+				this.io.emit('bandList', this.bandList.getBands());
+			});
+
+			socket.on('addBand', (name) => {
+				this.bandList.addBand(name);
+				this.io.emit('bandList', this.bandList.getBands());
 			});
 		});
 	}
